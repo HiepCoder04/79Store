1b52eae60b93419b57f7c4a6e9751abd856a1578
HiepCoder04 <hiepn1479@gmail.com>
2025-06-06 14:21:59 +0700

    giao diện admin

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\Admin\ProductController;
+// Admin
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.'
+], function () {
+    
+    // ADMIN
+    Route::group([
+        'prefix' => 'products',
+        'as' => 'products.'
+    ], function () {
+        
+        Route::get('/', [ProductController::class, 'listProducts'])->name('listProducts');
+        
+    });
 });
