1bfc83155e4cfd950643a768abf1d38cd0a408ab
sancpph38546 <sancpph38546@fpt.edu.vn>
2025-07-12 15:12:29 +0700

    quen mat khau 1

---
+++ resources/views/emails/password-otp.blade.php
@@ -0,0 +1,5 @@
+<p>Xin chào,</p>
+<p>Mã OTP để đặt lại mật khẩu của bạn là:</p>
+<h2>{{ $otp }}</h2>
+<p>Mã này có hiệu lực trong 5 phút.</p>
+<p>Nếu bạn không yêu cầu, vui lòng bỏ qua email này.</p>
