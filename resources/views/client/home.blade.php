56d680c182370e08b7659fbd278727e3f2132451
GIANGDEPTRAI88 <giangkoi270604@gmail.com>
2025-07-17 00:33:18 +0700

    fix hình ảnh

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,5 +1,16 @@
 @extends('client.layouts.default')
 
+
+<style>
+.product-img img {
+    width: 100%;
+    height: 250px;
+    /* hoặc chiều cao tùy ý */
+    object-fit: cover;
+    object-position: center;
+    border-radius: 8px;
+}
+</style>
 @section('title', 'Trang chủ')
 
 @section('content')
@@ -10,5 +21,6 @@
 @include('client.layouts.products')
 @include('client.layouts.blog')
 @include('client.layouts.subscribe')
+
 
 @endsection
