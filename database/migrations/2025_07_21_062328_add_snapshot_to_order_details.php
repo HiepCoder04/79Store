64973b3859fbf8f31af34795513f30a67a50a067
NgocAnh07 <ngoc20055anh@gmail.com>
2025-07-21 13:29:03 +0700

    fix don hang

---
+++ database/migrations/2025_07_21_062328_add_snapshot_to_order_details.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::table('order_details', function (Blueprint $table) {
+        
+        $table->decimal('product_price', 10, 2);
+        $table->string('product_height')->nullable();
+        $table->string('product_pot')->nullable();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+        });
+    }
+};
