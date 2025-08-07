7b4da778824fca51afd7f454b043705ba4ac168a
GIANGDEPTRAI88 <giangkoi270604@gmail.com>
2025-08-07 13:53:27 +0700

    bieudothongke

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,31 +5,38 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
-
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 class DashboardController extends Controller
 {
     public function index(Request $request)
-    {
-        $start = $request->input('start_date');
-        $end = $request->input('end_date');
+    {   
+ 
+$soDonHangTheoNgay =DB::table('orders')
+    ->selectRaw('DATE(created_at) as date, COUNT(*) as total')
+    ->where('status', 'delivered') // có thể bỏ nếu muốn tính mọi trạng thái
+    ->groupByRaw('DATE(created_at)')
+    ->orderBy('date', 'asc')
+    ->get();
 
-        // Query đơn hàng
-        $query = Order::query();
+   $doanhThus = DB::table('order_details')
+    ->join('orders', 'order_details.order_id', '=', 'orders.id')
+    ->where('orders.status', 'delivered') // hoặc điều kiện khác
+    ->selectRaw('DATE(orders.created_at) as date, SUM(order_details.price * order_details.quantity) as total')
+    ->groupByRaw('DATE(orders.created_at)')
+    ->orderBy('date', 'asc')
+    ->get();
 
-        // Nếu lọc theo khoảng ngày
-        if ($start && $end) {
-            $query->whereBetween('created_at', [$start, $end]);
-        }
 
-        $orders = $query->get();
+        $doanhThu = DB::table('order_details')
+            ->join('orders', 'order_details.order_id', '=', 'orders.id')
+            ->where('orders.status', 'delivered')
+            ->select(DB::raw('SUM(order_details.price * order_details.quantity) as total'))
+            ->value('total');   
+        $donHangChoXuLy =Order::where('status', 'pending')->count();
+        $donHangDaGiao = Order::where('status', 'delivered')->count();
+        $donHangDaHuy = Order::where('status', 'cancelled')->count();
 
-        // Tính tổng số đơn và tổng doanh thu
-        $totalOrders = $orders->count();
-        $totalRevenue = $orders->sum('total');
-
-        // Trả dữ liệu ra giao diện dashboard
-        return view('admin.dashboard', compact(
-            'orders', 'totalOrders', 'totalRevenue', 'start', 'end'
-        ));
+        return view('admin.dashboard', compact('doanhThus','soDonHangTheoNgay','doanhThu', 'donHangChoXuLy', 'donHangDaGiao', 'donHangDaHuy'));
     }
 }
